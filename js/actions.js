cc0e12a92f87dc10b49c9bb26c032d4148382c86
Nick Novak <novak1nt@gmail.com>
2015-08-05 13:30:42 -0400

    headroom integration
    Adding a plugin called Headroom.js to the portfolio site.
    https://github.com/WickyNilliams/headroom.js

--- js/actions.js
+++ js/actions.js
@@ -1,54 +1,42 @@
 // ------------------------------//
-// ------ Top Nav anchoring -----//
+// ------ Smooth Scrolling ------//
 // ------------------------------//
 
+
 $(function() {
-         
-    // get initial top offset of navigation 
-    var floating_navigation_offset_top = $('#nav').offset().top;
-             
-    // define the floating navigation function
-    var floating_navigation = function(){
-                // current vertical position from the top
-        var scroll_top = $(window).scrollTop(); 
-         
-        // if scrolled more than the navigation, change its 
-                // position to fixed to float to top, otherwise change 
-                // it back to relative
-        if (scroll_top > floating_navigation_offset_top) { 
-            $('#nav').css({ 'position': 'fixed', 'top':0});
-        } else {
-            $('#nav').css({ 'position': 'relative' }); 
-        }   
-    };
-     
-    // run function on load
-    floating_navigation();
-     
-    // run function every time you scroll
-    $(window).scroll(function() {
-         floating_navigation();
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
     });
- 
+
+    $('li a.nav').click(function(){
+        $('a.nav').removeClass("active");
+        $(this).addClass("active");
+    });
+
+// ------------------------------//
+// ------ Headroom Script -----//
+// ------------------------------//
+
+$("header").headroom({
+  "offset": 305,
+  "tolerance": 5,
+  "classes": {
+    "initial": "animated",
+    "pinned": "slide--Down",
+    "unpinned": "slide--Up"
+  }
 });
 
-//-------------------------------//
-//----------- Top Nav -----------//
-//-------------------------------//
-
-// var previousScroll = 0,
-//     headerOrgOffset = $('#nav').height();
-
-// $('#nav--Wrapper').height($('#nav').height());
-
-// $(window).scroll(function () {
-//     var currentScroll = $(this).scrollTop();
-//     if (currentScroll > headerOrgOffset) {
-//         if (currentScroll > previousScroll) {
-//             $('#nav--Wrapper').slideUp();
-//         } else {
-//             $('#nav--Wrapper').slideDown();
-//         }
-//     } 
-//     previousScroll = currentScroll;
-// });
+// to destroy
+$("#header").headroom("destroy");
